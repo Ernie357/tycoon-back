63477a4ed89e8710e40ac82a2b93fdb12ecea4f5
John <jmars051@live.kutztown.edu>
2024-11-06 04:32:30 -0500

    made cord origin '*'

--- src/server.ts
+++ src/server.ts
@@ -28,10 +28,7 @@ interface UserSocket extends Socket {
 
 const app: Express = express();
 
-app.use(cors({
-  origin: 'https://tycoon.onrender.com',
-  methods: ['GET', 'POST']
-}));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -39,8 +36,8 @@ const port = process.env.PORT || 5000;
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://tycoon.onrender.com',
-    methods: ['GET', 'POST']
+    origin: '*',
+    methods: ['GET', 'POST'],
   }
 });
 //app.use(express.static(path.join(__dirname, '../front/build')));
