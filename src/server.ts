e0b20ef74065f04a8388c01e60272cef7f1dfe47
John <jmars051@live.kutztown.edu>
2024-11-06 04:48:21 -0500

    added localhost to origin

--- src/server.ts
+++ src/server.ts
@@ -28,19 +28,15 @@ interface UserSocket extends Socket {
 
 const app: Express = express();
 
-app.use(cors({
-  origin: 'https://tycoon.onrender.com',
-  methods: ['GET', 'POST']
-}));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://tycoon.onrender.com',
-    methods: ['GET', 'POST'],
+    origin: ['http://localhost:5000', 'https://tycoon.onrender.com']
   }
 });
 //app.use(express.static(path.join(__dirname, '../front/build')));
@@ -133,5 +129,5 @@ app.get('*', (req, res) => {
 */
 
 server.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at ${port}`);
 });
