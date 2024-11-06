0ee366958acf169124ac1410a11a8c600170138b
John <jmars051@live.kutztown.edu>
2024-11-06 04:34:34 -0500

    reverted cors origins

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,10 @@ interface UserSocket extends Socket {
 
 const app: Express = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://tycoon.onrender.com',
+  methods: ['GET', 'POST']
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -36,7 +39,7 @@ const port = process.env.PORT || 5000;
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'https://tycoon.onrender.com',
     methods: ['GET', 'POST'],
   }
 });
