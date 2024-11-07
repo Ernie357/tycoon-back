5bfa50d43a68d055e4507677d5aebd850f05860f
John <jmars051@live.kutztown.edu>
2024-11-07 05:02:51 -0500

    idek anymore

--- src/server.ts
+++ src/server.ts
@@ -27,9 +27,10 @@ interface UserSocket extends Socket {
 }
 
 const app: Express = express();
+const frontUrl = process.env.FRONT_URL || "https://personatycoon.netlify.app";
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://personatycoon.netlify.app', 'https://personatycoon.com', 'https://www.personatycoon.com']
+  origin: frontUrl
 }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -38,7 +39,7 @@ const port = process.env.PORT || 5000;
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', 'https://personatycoon.netlify.app', 'https://personatycoon.com', 'https://www.personatycoon.com']
+    origin: frontUrl
   }
 });
 //app.use(express.static(path.join(__dirname, '../front/build')));
