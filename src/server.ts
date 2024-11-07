e0386a24d1b7c6d9b62f3edf113d30e4cdbc6772
John <jmars051@live.kutztown.edu>
2024-11-07 04:02:21 -0500

    added netlify cors

--- src/server.ts
+++ src/server.ts
@@ -36,7 +36,7 @@ const port = process.env.PORT || 5000;
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', 'https://tycoon.onrender.com', 'https://personatycoon.com']
+    origin: ['http://localhost:3000', 'https://personatycoon.netlify.app', 'https://personatycoon.com']
   }
 });
 //app.use(express.static(path.join(__dirname, '../front/build')));
